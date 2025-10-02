f0410767c7148bb22ec7a727bb650b964c65727c
Javier Chavez <cjavier0315@gmail.com>
2025-10-02 15:56:50 -0400

    add config and sessions

---
+++ app/config.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+BASE_URL = "http://127.0.0.1:8000"
+REDIRECT_URI = f"{BASE_URL}/dashboard"
+
+
+STRAVA_AUTHORIZE_URL = "https://www.strava.com/oauth/authorize"
+STRAVA_TOKEN_URL = "https://www.strava.com/oauth/token"
+STRAVA_API_BASE_URL = "https://www.strava.com/api/v3"
+
+SESSION_COOKIE_NAME = "strava_session_id"
+
+# Validate required environment variables
+if not CLIENT_ID or not CLIENT_SECRET:
+    raise ValueError("CLIENT_ID and CLIENT_SECRET must be set in .env file")
---
+++ app/sessions.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from typing import Optional, Dict
+import secrets
+from fastapi import HTTPException
+
+_session_store: Dict[str, dict] = {}
+
+def create_session(user_info: dict, strava_access_token: str, 
+                   strava_refresh_token: str, expires_at: datetime) -> str:
+    session_id = secrets.token_urlsafe(32)
+
+    _session_store[session_id] = {
+        "user_info": user_info,
+        "strava_access_token" : strava_access_token,
+        "strava_refresh_token" : strava_refresh_token,
+        "expires_at" : expires_at
+    }
+    return session_id
+    
+
+def get_session(session_id: str) -> Optional[dict]:
+    return _session_store.get(session_id)
+
+def delete_session(session_id: str) -> bool:
+    if session_id in _session_store:
+        del _session_store[session_id]
+        return True
+    return False
